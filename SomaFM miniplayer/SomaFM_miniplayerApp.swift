b3129174f91dca685c2e5f2d6f8cb06a274d9c22
James Greenhill <fuziontech@gmail.com>
2025-07-05 17:47:53 -0700

    Initial Commit

---
+++ SomaFM miniplayer/SomaFM_miniplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomaFM_miniplayerApp.swift
+//  SomaFM miniplayer
+//
+//  Created by James Greenhill on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SomaFM_miniplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SomaFM miniplayerTests/SomaFM_miniplayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SomaFM_miniplayerTests.swift
+//  SomaFM miniplayerTests
+//
+//  Created by James Greenhill on 7/5/25.
+//
+
+import Testing
+@testable import SomaFM_miniplayer
+
+struct SomaFM_miniplayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
